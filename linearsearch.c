b559c0237249a772c7afdd116e4a363624eb733c
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-05-25 20:59:02 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
-// Linear Search in array using C...
-// Made By Aditya Lawate on 18/5/22
-// at 22:46 pm.
-int linear_search(int arr[], int size, int num)
-{
-    int i, flag = 0, k;
-    for (i = 0; i < size; i++)
-    {
-        if (arr[i] == num)
-        {
-            flag = 1;
-            k = arr[i];
-            break;
-        }
-    }
-    if (flag == 0)
-        return -1;
-    else
-        return i;
-}
+
+/* Linear Search in array using C...
+ Made By Aditya Lawate on 18/5/22
+ at 22:46 pm.
+*/
 
 int main()
 {
-    int arr[10], i, n;
-    for (i = 0; i < 10; i++)
-        scanf("%d", &arr[i]);
+    int flag = 0, i, n;
+    int arr[] = {1, 96, 89, 23, 45, 67, 877, 654, 6543, 56, 76, 43};
+
     printf("Enter a number to be searched:\n");
     scanf("%d", &n);
-    int k = linear_search(arr, *(&arr + 1) - arr, n);
-    printf("%d", k);
+
+    for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        if (arr[i] == n)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("-1");
+    else
+        printf("The Number %d was found at position %d", n, 1 + i);
     return 0;
 }
