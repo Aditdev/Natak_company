7a75c52c5a09e87f1b304813ddee4aff0b324468
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-07-10 13:38:09 +0530

    Commit

--- bubblesort.c
+++ bubblesort.c
@@ -10,7 +10,7 @@ int main()
     int a[] = {1, 76, 54, 678, 544, 356, 5788, 2568, 244454, 76554, 3534, 3323, 565};
     for (i = 0; i < sizeof(a) / sizeof(int); i++)
     {
-        for (j = 0; j < sizeof(a) / sizeof(int); j++)
+        for (j = 0; j < sizeof(a) / sizeof(int)i-1; j++)
         {
             if (a[j] > a[j + 1])
             {
